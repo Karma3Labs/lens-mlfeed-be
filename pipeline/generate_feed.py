e283de9fc7e7760b582fd97262c2c7d66eaafdda
Vijay Mariadassou <vijay@karma3labs.com>
2023-05-31 09:17:10 -0700

    sampling in time bins to surface recent posts

--- pipeline/generate_feed.py
+++ pipeline/generate_feed.py
@@ -1,8 +1,11 @@
 import time
+from datetime import datetime, timezone
 import argparse
+import numpy as np
+import pandas as pd
 from sqlalchemy import create_engine
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import lit, monotonically_increasing_id
+from pyspark.sql.functions import col, when
 
 parser = argparse.ArgumentParser()
 
@@ -27,32 +30,42 @@ df = df.where(f"recommend != 'NO'")
 df = df.select("post_id", "recommend", "dtime")
 df.printSchema()
 
-total_yes = df.select('post_id').where(df.recommend == 'YES').count()
-print(f"total_yes:{total_yes}")
-      
-total_maybe = df.select('post_id').where(df.recommend == 'YES').count()
-print(f"total_maybe:{total_maybe}")
+dtime_now = int(datetime.utcnow().replace(microsecond=0).strftime("%Y%m%d%H%M%S"))
+print(f"dtime_now:{dtime_now}")
 
-# we need 100 rows but sampling sometimes returns less than 100; start with 120
-num_yes = 0.8*120
-num_maybe = 0.2*120
+condn_1day = dtime_now - col("dtime") < 86400
+condn_7day = dtime_now - col("dtime") < 604800
+condn_30day = dtime_now - col("dtime") < 2592000
 
-yes_fraction = round(num_yes / total_yes, 10)
-print(f"yes_fraction:{yes_fraction}")
+df = df.withColumn("time_ago",
+                   when(condn_1day , "1d") \
+                   .when(condn_7day, "7d") \
+                   .when(condn_30day, "30d") \
+                   .otherwise("99d"))
+print("converting Pyspark dataframe to Pandas")
+pd_df = df.toPandas()
+print('YES', pd_df[pd_df['recommend'] == 'YES']['time_ago'].value_counts())
+print('MAYBE', pd_df[pd_df['recommend'] == 'MAYBE']['time_ago'].value_counts())
 
-maybe_fraction = round(num_maybe / total_maybe, 10)
-print(f"maybe_fraction:{maybe_fraction}")
+rng = np.random.default_rng()
+samples = []
+counts = pd_df['time_ago'].value_counts()
+samples.append(pd_df.loc[pd_df['time_ago'] == '1d'].sample(n=min(counts['1d'],100), random_state=rng))
+samples.append(pd_df.loc[pd_df['time_ago'] == '7d'].sample(n=min(counts['7d'],50), random_state=rng))
+samples.append(pd_df.loc[pd_df['time_ago'] == '30d'].sample(n=min(counts['30d'],50), random_state=rng))
+samples.append(pd_df.loc[pd_df['time_ago'] == '99d'].sample(n=min(counts['99d'],50), random_state=rng))
+sample_df = pd.concat(samples)
+print('Sampled', sample_df['time_ago'].value_counts())
 
-seed = round(time.time()*1000)
-sample_df = df.sampleBy("recommend", fractions={'YES': yes_fraction, 'MAYBE': maybe_fraction}, 
-                        seed=seed)
+sample_df['weights'] = np.where(sample_df['recommend'] == 'YES', .8, .2)
+sample_df = sample_df.sample(n=100, weights='weights', random_state=rng)
+print('Recommend', sample_df['recommend'].value_counts())
 
-sample_pd_df =  sample_df.toPandas()
-sample_pd_df['strategy_name'] = "ml-xgb-followship"
-sample_pd_df = sample_pd_df.sort_values(['dtime'], ascending=[False], ignore_index=True)
-sample_pd_df['v'] = sample_pd_df.index
-print(sample_pd_df.head())
-sample_pd_df.drop(['recommend', 'dtime'], axis=1, inplace=True)
+sample_df = sample_df.sort_values(['dtime'], ascending=[False], ignore_index=True)
+sample_df = sample_df[['post_id']]
+sample_df['strategy_name'] = "ml-xgb-followship"
+sample_df['v'] = sample_df.index
+print(sample_df.head())
 
 SQLALCHEMY_SILENCE_UBER_WARNING=1
 db = create_engine(args.pgsql_url);
@@ -60,4 +73,4 @@ print("Deleting older feed entries for strategy 'ml-xgb-followship'")
 db.execute("DELETE FROM feed WHERE strategy_name = 'ml-xgb-followship'")
 
 print("Inserting new feed entries for strategy 'ml-xgb-followship'")
-sample_pd_df.iloc[:100].to_sql('feed', con=db, if_exists='append', index=False)
+sample_df.to_sql('feed', con=db, if_exists='append', index=False)
