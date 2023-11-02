8d8669644344f1082df5d323d53f584ce77faa17
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-02 14:18:48 -0700

    renamed resource names to v2

--- pipeline/run_pipeline.sh
+++ pipeline/run_pipeline.sh
@@ -17,31 +17,31 @@ gcloud dataproc batches submit pyspark bq_to_gcs.py \
 --version=2.1 \
 --deps-bucket=gs://vijay-lens-dataproc-temp \
 --jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.30.0.jar \
--- "--bucket" "vijay-lens-bigquery-export"
+-- "--bucket" "vijay-lensv2-bigquery-export"
 
 # sleep otherwise Dataproc will throw a "Insufficient CPU Quota" Error
 echo "*** Sleeping zzzzzzzzzzzzzzzzzzzz ***"
 sleep 5m
 
 echo "*** GCS to Featurestore ***"
-gcloud dataproc batches submit pyspark posts_lens_features.py \
+gcloud dataproc batches submit pyspark gcs_to_features_publication.py \
 --account=eigen1-vijay-gcp@boxwood-well-386122.iam.gserviceaccount.com \
 --project=boxwood-well-386122 \
 --region=us-central1 \
---container-image="gcr.io/boxwood-well-386122/lens-recommender:latest" \
+--container-image="gcr.io/boxwood-well-386122/lensv2-recommender:latest" \
 --subnet=default-sub \
 --version=2.0 \
 --deps-bucket=gs://vijay-lens-dataproc-temp \
--- "--bucket" "vijay-lens-bigquery-export" \
+-- "--bucket" "vijay-lensv2-bigquery-export" \
 "--staging" "vijay-lens-feature-store-temp" \
-"-f" "lens_featurestore_t1"
+"-f" "lensv2_featurestore_t1"
 
 echo "*** GlobalTrust to Featurestore ***"
 source /home/vijay_karma3labs_com/venvs/lens-ml-venv/bin/activate
 # Uncomment next line only for the first run
 # python -m pip install --no-cache-dir --upgrade -r requirements.txt
 export GOOGLE_APPLICATION_CREDENTIALS=.eigen1-vijay-gcp.credentials.json
-python profiles_eigentrust_features.py -f lens_featurestore_t1
+python pg_to_features_globaltrust.py -f lensv2_featurestore_t1
 deactivate
 
 echo "*** Run predictions ***"
@@ -50,14 +50,14 @@ gcloud dataproc batches submit pyspark predict_posts.py \
 --project=boxwood-well-386122 \
 --region=us-central1 \
 --region=us-central1 \
---container-image="gcr.io/boxwood-well-386122/lens-recommender:latest" \
+--container-image="gcr.io/boxwood-well-386122/lensv2-recommender:latest" \
 --subnet=default-sub \
 --version=2.0 \
 --deps-bucket=gs://vijay-lens-dataproc-temp \
 -- "--staging" "vijay-lens-feature-store-temp" \
-"--source" "vijay-lens-bigquery-export" \
-"--mlbucket" "vijay-lens-ml" \
-"-f" "lens_featurestore_t1" \
+"--source" "vijay-lensv2-bigquery-export" \
+"--mlbucket" "vijay-lensv2-ml" \
+"-f" "lensv2_featurestore_t1" \
 "--modelversion" "20230522053757"
 
 # sleep otherwise Dataproc will throw a "Insufficient CPU Quota" Error
@@ -71,11 +71,11 @@ gcloud dataproc batches submit pyspark generate_feed.py \
 --project=boxwood-well-386122 \
 --region=us-central1 \
 --region=us-central1 \
---container-image="gcr.io/boxwood-well-386122/lens-recommender:latest" \
+--container-image="gcr.io/boxwood-well-386122/lensv2-recommender:latest" \
 --subnet=default-sub \
 --version=2.0 \
 --deps-bucket=gs://vijay-lens-dataproc-temp \
--- "--gcspath" "gs://vijay-lens-ml/predictions/20230522053757_xgbcl/" \
+-- "--gcspath" "gs://vijay-lensv2-ml/predictions/20230522053757_xgbcl/" \
 "--pgsql-url" $PGSQL_URL 
 
 gcloud config set account $PREV_GCP_ACCOUNT
