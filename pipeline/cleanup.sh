50b72d1bbcd66f1722365c89b9c14191a618c71a
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-06 11:28:07 -0800

    added script to cleanup regularly

---
+++ pipeline/cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+printf "\n\n******Starting Cleanup*"
+date
+
+printf "\n\nDeleting SUCCEEDED batches a week ago"
+WEEKAGO=`date -d "1 week ago" +"%Y-%m-%d"`
+for batch in $(gcloud dataproc batches list --region=us-central1 --limit=10 --filter="create_time<$AGO AND state=SUCCEEDED" --format='value(name)'); 
+do 
+  $(gcloud dataproc batches delete --quiet $batch --region=us-central1); 
+done
+
+# we want to retain failures for a month to give us time to investigate
+printf "\n\nDeleting FAILED batches a month ago"
+MONTHAGO=`date -d "1 month ago" +"%Y-%m-%d"`
+for batch in $(gcloud dataproc batches list --region=us-central1 --limit=10 --filter="create_time<$AGO AND state=FAILED" --format='value(name)'); 
+do 
+  $(gcloud dataproc batches delete --quiet $batch --region=us-central1); 
+done
+
+printf "\n\n******Finished Cleanup*"
