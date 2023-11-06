6b11973a0674382fe552849ef3c893e8a66b4340
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-06 11:31:01 -0800

    increased cleanup limit to 1000

--- pipeline/cleanup.sh
+++ pipeline/cleanup.sh
@@ -5,7 +5,7 @@ date
 
 printf "\n\nDeleting SUCCEEDED batches a week ago"
 WEEKAGO=`date -d "1 week ago" +"%Y-%m-%d"`
-for batch in $(gcloud dataproc batches list --region=us-central1 --limit=10 --filter="create_time<$AGO AND state=SUCCEEDED" --format='value(name)'); 
+for batch in $(gcloud dataproc batches list --region=us-central1 --limit=1000 --filter="create_time<$AGO AND state=SUCCEEDED" --format='value(name)'); 
 do 
   $(gcloud dataproc batches delete --quiet $batch --region=us-central1); 
 done
@@ -13,7 +13,7 @@ done
 # we want to retain failures for a month to give us time to investigate
 printf "\n\nDeleting FAILED batches a month ago"
 MONTHAGO=`date -d "1 month ago" +"%Y-%m-%d"`
-for batch in $(gcloud dataproc batches list --region=us-central1 --limit=10 --filter="create_time<$AGO AND state=FAILED" --format='value(name)'); 
+for batch in $(gcloud dataproc batches list --region=us-central1 --limit=1000 --filter="create_time<$AGO AND state=FAILED" --format='value(name)'); 
 do 
   $(gcloud dataproc batches delete --quiet $batch --region=us-central1); 
 done
