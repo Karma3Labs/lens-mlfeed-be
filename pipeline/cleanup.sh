4f439a9eec119aea8a212d02f4b42f906cb386e1
Vijay Mariadassou <vijay@karma3labs.com>
2023-11-06 11:35:58 -0800

    fixed log statement newline

--- pipeline/cleanup.sh
+++ pipeline/cleanup.sh
@@ -3,7 +3,7 @@
 printf "\n\n******Starting Cleanup*"
 date
 
-printf "\n\nDeleting SUCCEEDED batches a week ago"
+printf "\n\nDeleting SUCCEEDED batches a week ago\n"
 WEEKAGO=`date -d "1 week ago" +"%Y-%m-%d"`
 for batch in $(gcloud dataproc batches list --region=us-central1 --limit=1000 --filter="create_time<$WEEKAGO AND state=SUCCEEDED" --format='value(name)'); 
 do 
@@ -11,7 +11,7 @@ do
 done
 
 # we want to retain failures for a month to give us time to investigate
-printf "\n\nDeleting FAILED batches a month ago"
+printf "\n\nDeleting FAILED batches a month ago\n"
 MONTHAGO=`date -d "1 month ago" +"%Y-%m-%d"`
 for batch in $(gcloud dataproc batches list --region=us-central1 --limit=1000 --filter="create_time<$MONTHAGO AND state=FAILED" --format='value(name)'); 
 do 
